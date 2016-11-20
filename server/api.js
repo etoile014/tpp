e7b96daf457ab0cf1aafd87fda7815a9b7078ce4
Yuki.K <je6hbc@d-io.com>
2016-11-20 05:06:41 +0000

    exact credit sum and charCode fixed kdb sqlite add

--- server/api.js
+++ server/api.js
@@ -2,10 +2,16 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
+var jschardet = require('jschardet');
+
+//backend
+var co = require('co');
+var sleep = require('sleep-async')();
 
 //connect to sqliteDB
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(process.cwd() + '/database/requirements.db');
+var courseDB = new sqlite3.Database(process.cwd() + '/database/kdb.db');
 
 app.set('views', process.cwd()+ '/views');
 var ejsEngine = require('ejs');
@@ -17,7 +23,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-console.log(db.run("select *  from department"));
+//test sqlite serverconnection
+console.log(db.run('select *  from department'));
+/*
+courseDB.get("select * from course", function(err, row) {
+    console.log(row.name);
+}
+);
+*/
 
 //Server for lisning socket.
 var server = app.listen(80, function(){
@@ -26,67 +39,160 @@ var server = app.listen(80, function(){
 
 //api get method
 app.get("/api/csv", function(req, res, next){
+    var temp;
+    var temp2;
+    var charCode;
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("hello guys! I got post methot!!");
+    courseDB.get('select name as name,teacher from course limit 10', function(err, row) {
+	temp = row.teacher;
+	temp2 = row.name;
+	charCode = jschardet.detect(temp);
+	console.log(temp2 + ": " + temp);
+    });
+    res.write(temp2 + ":" + temp + "//" + charCode);
     res.end();
-    console.log("app.get launch.");
-});
+    console.log("app.get launch");
+    });
 
 //api post method
 app.post("/api/csv", function(req, res, next){
-    var str = req.body;
-    var totalAA = 0;
-    var totalA = 0;
-    var totalB = 0;
-    var totalC = 0;
-    var totalD = 0;
-    var totalX = 0;
-    var totalP = 0;
-    var totalF = 0;
+    //init post function local handler
+    var total = [0,0,0,0,0,0,0,0,0,0,0];//a+,a,b,c,d,x,p,f
+    var semesterTotal = [0,0,0,0,0,0,0];
+    var semesterGPA = [0,0,0,0,0,0,0];
 
     console.log("GOT JSON!!");
     
     res.contentType('application/json');
-    for (var i=0; eval("req.body.line" + i) != undefined ; i++){
-	if(eval("req.body.line" + i + ".grade") == "A+"){totalAA++}
-	if(eval("req.body.line" + i + ".grade") == "A"){totalA++}
-	if(eval("req.body.line" + i + ".grade") == "B"){totalB++}
-	if(eval("req.body.line" + i + ".grade") == "C"){totalC++}
-	if(eval("req.body.line" + i + ".grade") == "D"){totalD++}
-	if(eval("req.body.line" + i + ".grade") == "X"){totalX++}
-	if(eval("req.body.line" + i + ".grade") == "P"){totalP++}
-	if(eval("req.body.line" + i + ".grade") == "F"){totalF++}
-    }
-
-    db.each("SELECT min, max from common_compulsory, department where subject = '総合1' and department.departmentID=common_compulsory.departmentID and department.department_name like '%創成%'", function(err, row) {
-	var x = 0, y = 0;
-	if(row.min == row.max) {
-	    x = row.min;
+    db.serialize(function () {
+	co(function *() {
 	    for (var i=0; eval("req.body.line" + i) != undefined ; i++){
-		var str = eval("req.body.line" + i + ".subject");
-		if( str.match(/1119/) || str.match(/1319/) || str.match(/12/)){
-		    y++;
+		
+		console.log(eval("req.body.line" + i + ".year"));
+		switch(eval("req.body.line" + i + ".year")) {
+		case "2013":
+		    check2013(i, eval("req.body.line" + i + ".subject"), eval("req.body.line" + i + ".grade"), total);
+		    break;
+		case "2014":
+		    check2014(i, eval("req.body.line" + i + ".subject"), eval("req.body.line" + i + ".grade"), total);
+		    break;
+		case "2015":
+		    check2015(i, eval("req.body.line" + i + ".subject"), eval("req.body.line" + i + ".grade"), total);
+		    break;
+		case "2016":
+		    check2016(i, eval("req.body.line" + i + ".subject"), eval("req.body.line" + i + ".grade"), total);
+		    break;
+		}
+		
+	    }
+	    });
+    });
+		 
+	db.each("SELECT min, max from common_compulsory, department where subject = '総合1' and department.departmentID=common_compulsory.departmentID and department.department_name like '%創成%'", function(err, row) {
+	    var x = 0, y = 0;
+	    if(row.min == row.max) {
+		x = row.min;
+		for (var i=0; eval("req.body.line" + i) != undefined ; i++){
+		    var str = eval("req.body.line" + i + ".subject");
+		    if( str.match(/1119/) || str.match(/1319/) || str.match(/12/)){
+			y++;
+		    }
 		}
 	    }
 	}
-    }
-	   )
-
-    console.log("-analyzed");
-    
-    var resData = [
-	{'name': 'A+', 'data': totalAA},
-	{'name': 'A', 'data': totalA},
-	{'name': 'B', 'data': totalB},
-	{'name': 'C', 'data': totalC},
-	{'name': 'D', 'data': totalD},
-	{'name': 'X', 'data': totalX},
-	{'name': 'P', 'data': totalP},
-	{'name': 'F', 'data': totalF},
-	{'name': 'completed', 'data': totalAA + totalA + totalB + totalC}
-    ];
-    var resDataJSON = JSON.stringify(resData);
-    res.send(resDataJSON);
-    res.end();
-    console.log("-JSON submitted to host!");
+	       )
+	    
+	    console.log("-analyzed");
+	
+    sleep.sleep(3000, function(){
+	var resData = [
+	    {'GRADE_GPA':{
+		'countAplus': total[0],
+		'countA': total[1],
+		'countB': total[2],
+		'countC': total[3],
+		'countD': total[4],
+		'countX': total[5],
+		'countP': total[6],
+		'countF': total[7],
+		'countOther': (total[5] + total[6] + total[7]),
+		'completed': (total[0] + total[1] + total[2] + total[3])
+	    }
+	    }
+	];
+	var resDataJSON = JSON.stringify(resData);
+	res.send(resDataJSON);
+	res.end();
+	console.log("-JSON submitted to host!");
+    });
+    //});
 });
+
+function check2013(i, subject, grade, total) {
+    courseDB.get('select credit,id from course2013 where id = ?',[subject],function (err, row){
+	if (err) {console.log(err)}
+	else {
+	    console.log(row);
+	    console.log("2013  " + i + grade);
+	    if(grade == "A+"){total[0] += row.credit}
+	    if(grade == "A"){total[1] += row.credit}
+	    if(grade == "B"){total[2] += row.credit}
+	    if(grade == "C"){total[3] += row.credit}
+	    if(grade == "D"){total[4] += row.credit}
+	    if(grade == "X"){total[5] += row.credit}
+	    if(grade == "P"){total[6] += row.credit}
+	    if(grade == "F"){total[7] += row.credit}
+	}
+    });
+}
+function check2014(i, subject, grade, total) {
+    courseDB.get('select credit,id from course2014 where id = ?',[subject],function (err, row){
+	if (err) {console.log(err)}
+	else {
+	    console.log(row);
+	    console.log("2014  " + i + grade);
+	    if(grade == "A+"){total[0] += row.credit}
+	    if(grade == "A"){total[1] += row.credit}
+	    if(grade == "B"){total[2] += row.credit}
+	    if(grade == "C"){total[3] += row.credit}
+	    if(grade == "D"){total[4] += row.credit}
+	    if(grade == "X"){total[5] += row.credit}
+	    if(grade == "P"){total[6] += row.credit}
+	    if(grade == "F"){total[7] += row.credit}
+	}
+    });
+}
+function check2015(i, subject, grade, total) {
+    courseDB.get('select credit,id from course2015 where id = ?',[subject],function (err, row){
+	if (err) {console.log(err)}
+	else {
+	    console.log(row);
+	    console.log("2015  " + i + grade);
+	    if(grade == "A+"){total[0] += row.credit}
+	    if(grade == "A"){total[1] += row.credit}
+	    if(grade == "B"){total[2] += row.credit}
+	    if(grade == "C"){total[3] += row.credit}
+	    if(grade == "D"){total[4] += row.credit}
+	    if(grade == "X"){total[5] += row.credit}
+	    if(grade == "P"){total[6] += row.credit}
+	    if(grade == "F"){total[7] += row.credit}
+	}
+    });
+}
+function check2016(i, subject, grade, total) {
+    courseDB.get('select credit,id from course2016 where id = ?',[subject],function (err, row){
+	if (err) {console.log(err)}
+	else {
+	    console.log(row);
+	    console.log("2016  " + i + grade);
+	    if(grade == "A+"){total[0] += row.credit}
+	    if(grade == "A"){total[1] += row.credit}
+	    if(grade == "B"){total[2] += row.credit}
+	    if(grade == "C"){total[3] += row.credit}
+	    if(grade == "D"){total[4] += row.credit}
+	    if(grade == "X"){total[5] += row.credit}
+	    if(grade == "P"){total[6] += row.credit}
+	    if(grade == "F"){total[7] += row.credit}
+	}
+    });
+}
